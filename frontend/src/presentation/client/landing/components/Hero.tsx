3d80bd3d8bde96090f1c2c4ddffea3648499ad7c
Jujunju <selamatgunakan@gmail.com>
2026-06-24 10:46:20 +0700

    feat: update data 5

--- frontend/src/presentation/client/landing/components/Hero.tsx
+++ frontend/src/presentation/client/landing/components/Hero.tsx
@@ -6,24 +6,24 @@ function Hero() {
     <div className="bg-white mb-0.5">
       <div className="flex flex-col md:flex-row md:items-center gap-5 py-10 px-5">
         <div>
-          <img src={heroImg} alt="" className="w-40 block shadow-lg" />
+          <img src={heroImg} alt="jujun" className="w-40 block shadow-lg" />
         </div>
         <div>
           <div className="mt-3">
-            <h1 className="text-2xl mb-3 font-mono">Hi, I'm Jujun Junaedi👋</h1>
-            <span className="">Software Engineer • MERN Stack • Clean Architecture Enthusiast.</span>
+            <h1 className="text-lg lg:text-2xl mb-3 font-mono">Hi, I'm Jujun Junaedi👋</h1>
+            <span className="text-sm lg:text-lg">Software Engineer • MERN Stack • Clean Architecture Enthusiast.</span>
           </div>
           <div className="flex gap-8 mt-5">
-            <a href="" className="block rounded-lg hover:-translate-y-1 transition-all duration-200">
+            <a href="https://github.com/Jujunju" className="block rounded-lg hover:-translate-y-1 transition-all duration-200">
               <Github size={20} />
             </a>
-            <a href="" className="blockrounded-lg hover:-translate-y-1 transition-all duration-200">
+            <a href="https://www.linkedin.com/in/jujun-junaedi-36b3833a2/" className="blockrounded-lg hover:-translate-y-1 transition-all duration-200">
               <Linkedin size={20} />
             </a>
-            <a href="" className="blockrounded-lg hover:-translate-y-1 transition-all duration-200">
+            <a href="https://www.youtube.com/@jnjunaedi" className="blockrounded-lg hover:-translate-y-1 transition-all duration-200">
               <Youtube size={20} />
             </a>
-            <a href="" className="blockrounded-lg hover:-translate-y-1 transition-all duration-200">
+            <a href="https://www.instagram.com/jnjunaedi/" className="blockrounded-lg hover:-translate-y-1 transition-all duration-200">
               <Instagram size={20} />
             </a>
           </div>
