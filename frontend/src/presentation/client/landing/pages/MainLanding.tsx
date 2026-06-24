091130628370cfcf40433a83045cf07990ef7075
Jujunju <selamatgunakan@gmail.com>
2026-06-24 08:43:12 +0700

    feat: update data delete import react 2

--- frontend/src/presentation/client/landing/pages/MainLanding.tsx
+++ frontend/src/presentation/client/landing/pages/MainLanding.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
 
 export default function MainLanding() {
   return (
